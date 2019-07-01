e8e58a49e750f9a31ba49b8bee34bbae4e097d9c
Vitor Albuquerque e Paiva <vitorapaiva@gmail.com>
2019-07-01 00:47:28 -0300

    Ajuste do posicionamento da classe model de User

---
+++ app/Http/Model/User/UserModel.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Model\User;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class UserModel extends Authenticatable implements JWTSubject
+{
+    use Notifiable;
+    protected $table = 'users';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email','company_id', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+}
