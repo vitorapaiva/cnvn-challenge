faf26d647dd02876ad9ce96e1e38c0efceda3d07
Vitor Albuquerque e Paiva <vitorapaiva@gmail.com>
2019-07-02 00:36:05 -0300

    Registro das interfaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('App\Http\Model\User\UserInterface', 'App\Http\Model\User\UserRepository');
+        $this->app->bind('App\Http\Model\Supplier\SupplierInterface', 'App\Http\Model\Supplier\SupplierApiRepository');
+        $this->app->bind('App\Http\Model\Company\CompanyInterface', 'App\Http\Model\Company\CompanyRepository');
+        //$this->app->bind('App\Http\Model\Supplier\SupplierInterface', 'App\Http\Model\Supplier\SupplierGraphRepository');
     }
 
     /**
