133be7894881fc7e4edc1092395020adb2f6aac7
Vitor Albuquerque e Paiva <vitorapaiva@gmail.com>
2019-07-02 00:36:27 -0300

    Novas rotas

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,18 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
-
     Route::post('authenticate', 'AuthController@authenticate')->name('api.authenticate');
     Route::post('register', 'AuthController@register')->name('api.register');
 });
 
+Route::group(['middleware' => ['api','auth'], 'prefix' => 'auth'], function () {
+    Route::get('authenticated', 'AuthController@getAuthenticatedUser')->name('api.authenticated');
+});
+
 Route::group(['middleware' => ['api','auth'],'prefix' => 'supplier'],function (){
-    Route::get('supplier/','SupplierController@add')->name('supplier.add');
+    Route::get('supplier/all','SupplierController@getAllSupplier')->name('supplier.getAllSupplier');
+    Route::get('supplier/get/{suppliers_id}','SupplierController@getSupplier')->name('supplier.getSupplier');
+    Route::post('supplier/','SupplierController@createSupplier')->name('supplier.createSupplier');
+    Route::put('supplier/{supplier_id}','SupplierController@editSupplier')->name('supplier.editSupplier');
+    Route::delete('supplier/{supplier_id}','SupplierController@deleteSupplier')->name('supplier.deleteSupplier');
 });
