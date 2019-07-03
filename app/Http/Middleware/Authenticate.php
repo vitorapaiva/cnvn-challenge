e25d9838537be0aed6251a29959a16536b249d1a
Vitor Albuquerque e Paiva <vitorapaiva@gmail.com>
2019-07-02 23:02:36 -0300

    Retorno de erro quando nao logado

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return response('Unauthenticated.', 401);
+            abort(401);
         }
     }
 }
